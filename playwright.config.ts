cd7054fd0c8c3adba82f479427453cad3e1da4e3
yutak23 <yutapilafu@gmail.com>
2023-12-21 04:58:40 +0000

    chore: set playwright webserver logs to output

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,9 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: `CONFIG_PATTERN=${process.env.CONFIG_PATTERN || 'default'} yarn build && yarn preview`,
-		port: 4173
+		port: 4173,
+		stdout: 'pipe',
+		stderr: 'pipe'
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
